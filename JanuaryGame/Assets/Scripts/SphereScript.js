44fa2d74afb2ccc12d49fc102e92a30fb2de6695
Sean Oxspring <Sean.Oxspring@hotmail.com>
2013-01-23 15:14:29 +0000

    Network Code Changes

--- JanuaryGame/Assets/Scripts/SphereScript.js
+++ JanuaryGame/Assets/Scripts/SphereScript.js
@@ -4,6 +4,12 @@ var newCol:Vector3 = Vector3(1,0,0);
 networkView.RPC("SetColor",RPCMode.AllBuffered,newCol);
 }
 
+function OnTriggerExit(){
+Debug.Log("Hello");
+var newCol:Vector3 = Vector3(0,1,0);
+networkView.RPC("SetColor",RPCMode.AllBuffered,newCol);
+}
+
 @RPC
 function SetColor(newColor:Vector3){
 renderer.material.color = Color(newColor.x,newColor.y,newColor.z,1);
