41a08e6ee7efea7d82d1b021a3a688fcc18ba521
Sean Oxspring <Sean.Oxspring@hotmail.com>
2013-01-23 16:41:06 +0000

    Revert "Deleted Scripts"
    This reverts commit aa91a4a7233a47046dcd79a44ccd715fc39076c7.

---
+++ JanuaryGame/Assets/Scripts/SphereScript.js
@@ -0,0 +1,16 @@
+function OnTriggerEnter(){
+Debug.Log("Hello");
+var newCol:Vector3 = Vector3(1,0,0);
+networkView.RPC("SetColor",RPCMode.AllBuffered,newCol);
+}
+
+function OnTriggerExit(){
+Debug.Log("Hello");
+var newCol:Vector3 = Vector3(0,1,0);
+networkView.RPC("SetColor",RPCMode.AllBuffered,newCol);
+}
+
+@RPC
+function SetColor(newColor:Vector3){
+renderer.material.color = Color(newColor.x,newColor.y,newColor.z,1);
+}
